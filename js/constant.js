912d5a0d813a2944f43bdac5d2202f9653405a76
Huynh Nguyen <yugivip277@gmail.com>
2023-09-28 20:56:31 +0700

    second-commit

--- js/constant.js
+++ js/constant.js
@@ -1,4 +1,4 @@
-const DOMAIN  = "https://project3-seven-theta.vercel.app/";
+const DOMAIN  = "https://project3-seven-theta.vercel.app";
 
 export const API_PRODUCT = DOMAIN + "/products";
 export const API_CATEGORY = DOMAIN + "/category";
